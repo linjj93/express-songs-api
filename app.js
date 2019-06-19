a6f8918157ce04afc5a82a26a13e9744cf68efdd
linjj93 <linjj93@hotmail.com>
2019-06-19 12:09:32 +0800

    Own practice

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let songs = [];
 
 //return list of all songs
-app.get('/songs', (req, res) => {
+app.get("/songs", (req, res) => {
   res.status(200).json(songs);
 });
 
 //create a new song, and return new song
-app.post('/songs', (req, res) => {
+app.post("/songs", (req, res) => {
   let newSong = {
     id: songs.length + 1,
     name: req.body.name,
-    artist: req.body.artist 
-  }
+    artist: req.body.artist
+  };
   songs.push(newSong);
   res.status(201).json(newSong);
 });
 
-//return a song with id 
-app.get('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id == parseInt(req.params.id));
+//return a song with id
+app.get("/songs/:id", (req, res) => {
+  const song = req.song;
+  // let song = songs.find(song => song.id == parseInt(req.params.id));
   res.status(200).json(song);
 });
 
 //edit a song with id, and return edited song
-app.put('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id === parseInt(req.params.id));
+app.put("/songs/:id", (req, res) => {
+  // let song = songs.find(song => song.id === parseInt(req.params.id));
+  const song = req.song;
   song.name = req.body.name;
   song.artist = req.body.artist;
   res.status(200).json(song);
@@ -37,10 +39,17 @@ app.put('/songs/:id', (req, res) => {
 
 //delete a song with id, and return deleted song
 app.delete("/songs/:id", (req, res) => {
-  let songToDelete = songs.find(song => song.id === parseInt(req.params.id));
+  // let songToDelete = songs.find(song => song.id === parseInt(req.params.id));
+  const songToDelete = req.song;
   let index = songs.indexOf(songToDelete);
   songs.splice(index, 1);
   res.status(200).json(songToDelete);
 });
 
-module.exports = app
+app.param("id", (req, res, next, id) => {
+  const songToFind = songs.find(song => song.id === Number(id));
+  req.song = songToFind;
+  next();
+});
+
+module.exports = app;
